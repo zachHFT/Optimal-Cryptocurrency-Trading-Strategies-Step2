5c8ecfcff96e5eb1ce4b4a4a1526aa83ecb8fca1
Kai Cooper <kai.cooper19@imperial.ac.uk>
2021-11-18 15:53:26 +0100

    correlation analysis

---
+++ Reddit_Analysis/Price_Data_Extraction/Data/Binance_OHLC/correlation analysis.R
@@ -0,0 +1,14 @@
+library(stargazer)
+library(ggpubr)
+pnl_m <- read.csv('pnl_naji102_min.csv')
+pnl_hr <- read.csv('pnl_naji102_hr.csv')
+eth1m <- read.csv("ETHUSDT-1m-binance-imputed.csv")
+eth1hr <- read.csv("ETHUSDT-1h-binance-imputed.csv")
+
+cor.test(pnl_m$X0, eth1m$close)
+cor.test(pnl_hr$X, eth1hr$close)
+
+ggscatter(data=data.frame(y = pnl_hr$X0, x = eth1hr$close), x = "x", y = "y",
+          add = "reg.line", conf.int = TRUE, 
+          cor.coef = TRUE, cor.method = "pearson",
+          xlab = "PnL", ylab = "Close price")

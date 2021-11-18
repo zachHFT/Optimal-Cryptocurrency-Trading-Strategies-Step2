b53dec7f6b62486053d000b20f2804f93b73a9ae
Kai Cooper <kai.cooper19@imperial.ac.uk>
2021-11-18 15:59:08 +0100

    Corrected axes

--- Reddit_Analysis/Price_Data_Extraction/Data/Binance_OHLC/correlation analysis.R
+++ Reddit_Analysis/Price_Data_Extraction/Data/Binance_OHLC/correlation analysis.R
@@ -8,7 +8,7 @@ eth1hr <- read.csv("ETHUSDT-1h-binance-imputed.csv")
 cor.test(pnl_m$X0, eth1m$close)
 cor.test(pnl_hr$X, eth1hr$close)
 
-ggscatter(data=data.frame(y = pnl_hr$X0, x = eth1hr$close), x = "x", y = "y",
+ggscatter(data=data.frame(y = pnl_hr$X0, x = eth1hr$close), x = "y", y = "x",
           add = "reg.line", conf.int = TRUE, 
           cor.coef = TRUE, cor.method = "pearson",
           xlab = "PnL", ylab = "Close price") + ggtitle("Hourly close price against proft 'n' loss trajectory")
